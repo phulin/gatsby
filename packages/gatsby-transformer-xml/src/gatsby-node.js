1ad24605d28049d1e3fe4681277a693f834326e6
Patrick Hulin <patrick.hulin@gmail.com>
2019-05-07 00:20:04 -0400

    Replace xml-parser with xml2js.

--- packages/gatsby-transformer-xml/src/gatsby-node.js
+++ packages/gatsby-transformer-xml/src/gatsby-node.js
@@ -1,5 +1,15 @@
-const parseXml = require(`xml-parser`)
+const util = require(`util`)
+const xml2js = require(`xml2js`)
 const _ = require(`lodash`)
+
+function transformNode(obj) {
+  return {
+    name: obj[`#name`],
+    attributes: obj.$ || {},
+    content: obj._ || ``,
+    xmlChildren: obj.children ? obj.children.map(transformNode) : [],
+  }
+}
 
 async function onCreateNode({
   node,
@@ -15,17 +25,20 @@ async function onCreateNode({
     return
   }
   const rawXml = await loadNodeContent(node)
-  const parsedXml = parseXml(rawXml)
-  const nodeArray = parsedXml.root.children.map((obj, i) => {
-    if (obj.children) {
-      obj.xmlChildren = obj.children
-      delete obj.children
-    }
+  const parser = new xml2js.Parser({
+    explicitChildren: true,
+    preserveChildrenOrder: true,
+    childkey: `children`,
+    explicitRoot: false,
+  })
+  const parsedXml = await util.promisify(parser.parseString)(rawXml)
+  const nodeArray = parsedXml.children.map((obj, i) => {
     return {
-      ...obj,
-      id: obj.attributes.id
-        ? obj.attributes.id
-        : createNodeId(`${node.id} [${i}] >>> XML`),
+      ...transformNode(obj),
+      id:
+        obj.$ && obj.$.id
+          ? obj.$.id
+          : createNodeId(`${node.id} [${i}] >>> XML`),
       parent: node.id,
       children: [],
       internal: {
